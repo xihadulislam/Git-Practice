d2e1bcae8371e612743c83042a479cce914607d4
xihad <xihad.bd@gmail.com>
2020-06-02 13:40:33 +0600

    master commit

---
+++ app/src/main/java/com/okcodex/gitpractice/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.okcodex.gitpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        mainmaster("main master")
+
+    }
+
+
+
+    fun mainmaster(str : String){
+        Log.d("main",str)
+
+    }
+
+
+}
